c801b363191143baaae92623768cb299e7d35b49
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-02-25 19:48:52 -0300

    iniciar espaço palavras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import { useState } from "react"
 import alfabeto from "./alfabeto";
-//import palavras from "./palavras";
+import palavras from "./palavras";
 import Jogo from "./Jogo ";
 import Letras from "./Letras";
 
-export default  function App() {
-  const [HabilitarBotao, setHabilitador] = useState(false)
+export default function App() {
+  const [habilitarBotao, setHabilitarBotao] = useState(false);
   const [letrasHabilitadas, setLetrasHabilitadas] = useState(
     alfabeto.map((letra) => ({ letra, habilitado: false }))
   );
- 
+  const [tentativas, setTentativas] = useState(0);
 
   function habilitar() {
-    setHabilitador(!HabilitarBotao);
+    setHabilitarBotao(!habilitarBotao);
     setLetrasHabilitadas(
       letrasHabilitadas.map((letra) => ({ ...letra, habilitado: true }))
     );
   }
+
+  function incrementarTentativas() {
+    setTentativas(tentativas + 1);
+  }
+
+  function palavraAleatoria() {
+    const indiceAleatorio = Math.floor(Math.random() * palavras.length);
+    const palavra = palavras[indiceAleatorio];
+    const arrayPalavra = Array.from(palavra, () => "_");
+    return { palavra, arrayPalavra };
+  }
+
+
+
+
   return (
     <>
-       <Jogo habilitar={habilitar}/>
-       <Letras letrasHabilitadas={letrasHabilitadas} />
+      <Jogo
+        habilitar={habilitar}
+        tentativas={tentativas}
+      />
+      <Letras letrasHabilitadas={letrasHabilitadas} incrementarTentativas={incrementarTentativas} />
     </>
-  )
+  );
 }
--- src/Letras.js
+++ src/Letras.js
@@ -1,24 +1,22 @@
 import alfabeto from "./alfabeto";
 export default function Letras(props) {
-    function comer(comida) {
-        alert(` escolhi a letra ${comida} `);
-    }
     return (
-        <>
-            <div>
-                <div className="geladeira">
-                    {props.letrasHabilitadas.map((item) => (
-                        <button
-                            key={item.letra}
-                            onClick={() => comer(item.letra)}
-                            disabled={!item.habilitado}
-                            data-test="letter"
-                        >
-                            {item.letra}
-                        </button>
-                    ))}
-                </div>
-            </div>
-        </>
+      <>
+        <div>
+          <div className="geladeira">
+            {props.letrasHabilitadas.map((item) => (
+              <button
+                key={item.letra}
+                onClick={() => props.onLetraClick(item.letra)}
+                disabled={!item.habilitado}
+                data-test="letter"
+              >
+                {item.letra}
+              </button>
+            ))}
+          </div>
+        </div>
+      </>
     )
-}
+  }
+  
