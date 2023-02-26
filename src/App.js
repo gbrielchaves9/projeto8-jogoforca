0861804a1c3a8c40ec847325d4bf412a031b1c25
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-02-26 00:38:00 -0300

    quase pronto

--- src/App.js
+++ src/App.js
@@ -17,24 +17,47 @@ export default function App() {
     setLetrasHabilitadas(
       letrasHabilitadas.map((letra) => ({ ...letra, habilitado: true }))
     );
-    setPalavraOculta(palavraAleatoria().arrayPalavra);
+    setTentativas(0);
+    const palavraEscolhida = palavras[Math.floor(Math.random() * palavras.length)];
+    setPalavraOculta(Array(palavraEscolhida.length).fill('_'));
   }
 
-  function incrementarTentativas() {
-    setTentativas(tentativas + 1);
-  }
+  function onLetraClick(letra) {
+    const novaLetrasHabilitadas = letrasHabilitadas.map((item) => {
+      if (item.letra === letra) {
+        return { ...item, habilitado: false };
+      } else {
+        return item;
+      }
+    });
+    setLetrasHabilitadas(novaLetrasHabilitadas);
 
-  function palavraAleatoria() {
-    const indiceAleatorio = Math.floor(Math.random() * palavras.length);
-    const palavra = palavras[indiceAleatorio];
-    const arrayPalavra = Array.from(palavra, () => "_");
-    return { palavra, arrayPalavra };
+    if (!palavraOculta.includes(letra)) {
+      setTentativas(tentativas + 1);
+      if (tentativas >= 5) {
+        setLetrasHabilitadas(
+          letrasHabilitadas.map((letra) => ({ ...letra, habilitado: false }))
+        );
+      }
+    } else {
+      const indicesLetra = [];
+      for (let i = 0; i < palavraOculta.length; i++) {
+        if (palavraOculta[i] === letra) {
+          indicesLetra.push(i);
+        }
+      }
+      const novaPalavraOculta = [...palavraOculta];
+      for (const index of indicesLetra) {
+        novaPalavraOculta[index] = letra;
+      }
+      setPalavraOculta(novaPalavraOculta);
+    }
   }
 
   return (
     <>
-        <Jogo habilitar={habilitar} tentativas={tentativas} palavraOculta={palavraOculta} />
-      <Letras letrasHabilitadas={letrasHabilitadas} incrementarTentativas={incrementarTentativas} />
+      <Jogo habilitar={habilitar} tentativas={tentativas} palavraOculta={palavraOculta} />
+      <Letras letrasHabilitadas={letrasHabilitadas} onLetraClick={onLetraClick} />
     </>
   );
 }
